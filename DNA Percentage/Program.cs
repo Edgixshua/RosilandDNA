34f0a0e464377614592b1876bd147239caf97ff7
Joshua Sheffield <edgixshua@gmail.com>
2017-06-14 11:14:06 +0100

    Added solution files to DNA Percentage

---
+++ DNA Percentage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RosalindTemplate {
+    class Program {
+        [STAThreadAttribute]
+        static void Main(string[] args) {
+            // you'll need to update this filename for each problem.  Or do something cleverer..
+            string fileContents = File.ReadAllText(@"C:\Users\joshuas\Downloads\rosalind_gc.txt").Trim();
+            string result = Solver.Solve(fileContents);
+            Clipboard.SetText(result);
+            Console.WriteLine("Result:\n\n" + result);
+            Console.WriteLine("\nResult written to clipboard.");
+            //243 238 261 258
+            //252 268 214 245
+
+            Console.Read();
+        }
+    }
+}
