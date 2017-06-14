242f761e537d1db0960c71ae860bdc8ad5dbcfa5
Joshua Sheffield <edgixshua@gmail.com>
2017-06-14 11:01:31 +0100

    Added DNA Reverse folder

---
+++ DNA Reverse/Solver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RosalindTemplate {
+    public static class Solver {
+        public static string Solve(string input) {
+
+            var DnaGrouped = input.OrderBy(p => p).GroupBy(p => p);
+
+            var Numbers = DnaGrouped.Select(s => s.Count());
+
+            return string.Join(" ", Numbers.ToArray());
+
+            //var result= s.Select(x=> x == 'A' ? 'B' : (x=='B' ? 'A' : x)).ToArray();
+        }
+
+        private static String ReplaceChars(this string Text, char OldChar, char NewChar)
+
+            => string.Join("", Text.Select(x => x == 'A' ? 'B' : (x == 'B' ? 'A' : x)).ToArray());
+        
+    }
+}
---
+++ DNA Reverse/Solver_Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace RosalindTemplate {
+    [TestFixture]
+    public class Solver_Tests {
+        [Test]
+        public void Solve_GivenSomeInput_ProducesCorrectOutput() {
+            var input = "What is the answer?";
+
+            var expectedResult = "42";
+
+            Assert.That(Solver.Solve(input), Is.EqualTo(expectedResult));
+        }
+    }
+}
