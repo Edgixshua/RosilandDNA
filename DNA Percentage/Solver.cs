7ece9ec8e9f0c010275411710efdd0eccda21d18
Joshua Sheffield <edgixshua@gmail.com>
2017-06-14 11:12:59 +0100

    Added DNA Percentage folder to GitHub

---
+++ DNA Percentage/Solver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RosalindTemplate
+{
+    public static class Solver
+    {
+        public static string Solve(string input)
+        {
+            List<string> DnaList = new List<string>();
+            foreach (Match line in Regex.Matches(input, @">([A-Za-z0-9_]+)\s+([A-Za-z\s]+)"))
+            {
+                var DnaCapture = line.Groups[2].Value.Replace("\r\n", "").Trim();
+
+                var DnaCount = DnaCapture.Count(p => p == 'G' || p == 'C');
+
+                var Total = (((double)DnaCount / DnaCapture.Length) * 100);
+
+                DnaList.Add($"{line.Groups[1].Value}\r\n{Total}");
+            }
+
+            return string.Join("\r\n", DnaList.ToArray());
+        }
+    }
+}
