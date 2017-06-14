082de2cbf696bf79d4b1c7fd61e3530c3d3af2e2
Joshua Sheffield <edgixshua@gmail.com>
2017-06-14 11:06:33 +0100

    Added DNA Transcribing folder to GitHub

---
+++ DNA Transcribing/Solver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RosalindTemplate {
+    public static class Solver {
+        public static string Solve(string input) {
+
+            return string.Join("", input.Replace('C', 'x').Replace('G', 'C').Replace('x', 'G').Replace('A', 'z').Replace('T', 'A').Replace('z', 'T').Reverse().ToArray());
+            
+        }
+    }
+}
