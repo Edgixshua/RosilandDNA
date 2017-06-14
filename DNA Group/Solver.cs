13580f8cf3e965de51c8d7fbe02c212b55bf49a9
Joshua Sheffield <edgixshua@gmail.com>
2017-06-14 10:54:31 +0100

    Added DNA Group Folder

---
+++ DNA Group/Solver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RosalindTemplate {
+    public static class Solver {
+        public static string Solve(string input) {
+
+            var DnaGrouped = input.OrderBy(p => p).GroupBy(p => p);
+
+            var Numbers = DnaGrouped.Select(s => s.Count());
+
+            return string.Join(" ", Numbers.ToArray());
+        }
+    }
+}
